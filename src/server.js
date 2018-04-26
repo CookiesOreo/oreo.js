e348e37c91db6576dc4f8e270bc0e86e6a5fa76c
Arav Narula <37490238+Narulatools@users.noreply.github.com>
2018-04-26 18:54:38 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './client/App';
+import Html from './client/Html';
 
+const port = 1890;
+const server = express();
+
+server.get('/', (req, res) => {
+  
+  const body = renderToString(<App />);
+  const title = 'Server side Rendering with Styled Components';
+
+  res.send(
+    Html({
+      body,
+      title
+    })
+  );
+});
+
+server.listen(port);
+console.log(`https://github.com/CookiesOreo/oreo.js/edit/master/src/server.js`);
